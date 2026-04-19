128e33931faa4701df06f0599dae870f72db9a93
adamw101 <108877231+adamw101@users.noreply.github.com>
2026-04-19 20:02:20 +0200

    Update output filename and comments in script

--- scripts/generate_samples_lpf_tb.py
+++ scripts/generate_samples_lpf_tb.py
@@ -8,25 +8,25 @@ num_samples = 530
 
 # Plik wyjściowy
 
-filename = "/home/adam/lpf/sinus.csv"
+filename = "/path/sinus.csv"
 
 samples = []
 
 for n in range(num_samples):
-    # faza: 0 → 4*2π
+    # phase: 0 → 4*2π
     phase1 = 2 * math.pi * 2 * n / num_samples
     phase2 = 2 * math.pi * 100 * n / num_samples
 
-    # sinus w zakresie [-1, 1]
+    # sinus range [-1, 1]
     value = (math.sin(phase1) + 0.2*math.sin(phase2))/1.2
 
-    # skalowanie do int8 (-128..127)
+    # scaled down to int8 (-128..127)
     scaled = int(round(value * 127))
 
     samples.append(scaled)
 
 
-# zapis do CSV (jedna wartość na linię)
+# save to csv
 
 with open(filename, mode='w', newline='') as file:
     writer = csv.writer(file)
